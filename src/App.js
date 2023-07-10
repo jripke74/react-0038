1265331ace296227f9fde693ad2fca193a92ab79
Jeff Ripke <jripke74@gmail.com>
2023-07-10 10:19:48 -0700

    misc

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import { useState } from "react";
 
 import NewExpense from "./components/NewExpense/NewExpense";
 import Expenses from "./components/Expenses/Expenses";
+import CourseGoalList from "./components/CourseGoalList/CourseGoalList";
+import CourseInput from "./components/CourseInput/CourseInput";
+import "./App.css";
 
 const DUMMY_EXPENSES = [
   {
@@ -31,7 +34,36 @@ const DUMMY_EXPENSES = [
 ];
 
 const App = () => {
+  const [courseGoals, setCourseGoals] = useState([
+    { text: "Do all exercises!", id: "g1" },
+    { text: "Finish the course!", id: "g2" },
+  ]);
   const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
+
+  const addGoalHandler = (enteredText) => {
+    setCourseGoals((prevGoals) => {
+      const updatedGoals = [...prevGoals];
+      updatedGoals.unshift({ text: enteredText, id: Math.random().toString() });
+      return updatedGoals;
+    });
+  };
+
+  const deleteItemHandler = (goalId) => {
+    setCourseGoals((prevGoals) => {
+      const updatedGoals = prevGoals.filter((goal) => goal.id !== goalId);
+      return updatedGoals;
+    });
+  };
+
+  let content = (
+    <p style={{ textAlign: "center" }}>No goals found. Maybe add one?</p>
+  );
+
+  if (courseGoals.length > 0) {
+    content = (
+      <CourseGoalList items={courseGoals} onDeleteItem={deleteItemHandler} />
+    );
+  }
 
   const addExpenseHandler = (expense) => {
     setExpenses((prevExpenses) => {
@@ -43,6 +75,10 @@ const App = () => {
     <div>
       <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses items={expenses} />
+      <section id="goals">
+        <CourseInput onAddGoal={addGoalHandler} />
+      </section>
+      <section id="goals">{content}</section>
     </div>
   );
 };
