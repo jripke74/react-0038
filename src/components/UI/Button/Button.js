1f0b870bab59eb2e87329be618166f8b58b446ed
Jeff Ripke <jripke74@gmail.com>
2023-07-11 12:41:10 -0700

    add

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,8 +1,12 @@
-import styles from "./Button.module.css";
+import classes from "./Button.module.css";
 
 const Button = (props) => {
   return (
-    <button type={props.type} className={styles.button} onClick={props.onClick}>
+    <button
+      type={props.type || 'button'}
+      className={classes.button}
+      onClick={props.onClick}
+    >
       {props.children}
     </button>
   );
